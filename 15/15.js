24a4035c025157e4f269e9f807fad5c7cea48e23
geogra4 <klein.joshua.a@gmail.com>
2020-10-16 21:39:04 -0400

    problem 15

--- 15/15.js
+++ 15/15.js
@@ -5,13 +5,32 @@
 
 // Project Euler problem 15
 
-// create map
-var map = [];
-for (var i = 0; i < 20; i += 1) {
-    map[i] = [];
-    for (var j = 0; j < 20; j += 1) {
-        map[i][j] = 0;
+/* this appears to be some kind of combinatorics problem
+ * For a 2x2 grid, if you can only go right and down you need to have 2 right moves and 2 down moves in any order
+ * [R,R,D,D]
+ * [D,D,R,R]
+ * [R,D,R,D]
+ * [D,R,D,R]
+ * [D,R,R,D]
+ * [R,D,D,R]
+ * 
+ * For a 3x3 grid you have 3 right moves and 3 down moves. 
+ * [D,D,D,R,R,R]
+ * [D,D,R,R,R,D]
+ * [...]
+ * The question asks for a 20x20 grid so 20 right moves and 20 down moves
+ * It's a combinatorics problem
+ * 
+ * Specifically a permutation of 40 different moves and 20 repititions of R and 20 repititions of D
+ * So 40!/(20!*20!)
+ */
+
+
+function factorial(x) {
+    if (x === 0) {
+        return 1;
     }
+    return x * factorial(x - 1);
 }
 
-while
+console.log(factorial(40) / (factorial(20) * factorial(20)));
